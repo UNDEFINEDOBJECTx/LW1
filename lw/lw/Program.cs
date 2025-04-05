367dac2acb09f6553b4976ef714c7c39b5247e42
Demid <demidelist@gmail.com>
2025-04-05 08:39:45 +0300

    adding

---
+++ lw/lw/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            while (a < 100000000)
+            {
+                a *= a;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
